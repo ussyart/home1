d9cb5e3fe32e3782306cbc8b58905ce093e41352
ussyart <95523346+ussyart@users.noreply.github.com>
2021-12-08 13:16:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -83,5 +83,27 @@ $(function(){
     // eqの引数をclickedIndexに書き換える
     $('.slide').eq(clickedIndex).addClass('active');
   });
+  
+  
+  /* タブレット・スマホで見たときにhoverを無効果する */
+var touch = 'ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
+if(touch) {
+  try {
+    for (var si in document.styleSheets) {
+      var styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) {}
+}
+  
 
 });
